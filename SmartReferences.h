569a8e0c7da9d6eb7c83c0601c2549f123702ec4
MaximB0nd <sersim11@yandex.ru>
2025-11-29 20:44:16 +0300

    feat: added SmartReferences.h

--- SmartReferences.h
+++ SmartReferences.h
@@ -5,12 +5,23 @@
 #ifndef CPPLESSONS_SMARTREFERENCES_H
 #define CPPLESSONS_SMARTREFERENCES_H
 
+#include <memory>
+
 class SmartReferences {
 	public:
 	static void info() {
 		int *number { new int { 5 }};
 		delete number;
 		number = nullptr;
+
+		int mass[] { new int[] {1, 3, 4}};
+
+		std::unique_ptr<int> value { std::make_unique<int>(5)};
+		int *pointer { value.get() };
+		value.reset(std::make_unique<int>(*pointer).get() );
+
+		std::shared_ptr<int> shared_ptr { std::make_shared<int>(123) };
+		std::shared_ptr<int> shared_ptr2 { shared_ptr };
 
 
 	}
